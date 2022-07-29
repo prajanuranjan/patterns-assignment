41ad04170075d1a4c2946008d3c2d69a92dfeee3
Prajanuranjan Sahoo <sahooprajanu@gmail.com>
2022-07-29 12:42:14 -0700

    Add files via upload

---
+++ ineuron.java
@@ -0,0 +1,95 @@
+package Assignment1;
+
+public class ineuron {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=7;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==0 || j==(n/2) ||i==n-1)
+					
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+	}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j==0 || i==j ||j==n-1)
+					
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+	}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(i==0 || i==(n/2) || i==n-1 || j==0)
+					
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+	}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if((j==0 && i!=(n-1)) || (j==n-1 && i!=(n-1)) || (i==n-1 && j!=0 && j!=n-1))
+					
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+	}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j==0 || i==(n-1)/2 || (i==0 && j!=n-1) || (j==n-1 && i<=(n-1)/2 && i!=0) || (i==j && i>(n-1)/2))
+					
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+	}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if((i==0 && j!=0 && j!=n-1) || (i==n-1 && j!=0 && j!=n-1) || (j==0 && i!=0 && i!=n-1) || (j==n-1 && i!=0 && i!=n-1))
+					
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+	}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(j==0 || i==j ||j==n-1)
+					
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+	}
+		 
+	}
+
+}
